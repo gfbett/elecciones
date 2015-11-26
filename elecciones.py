89593ff6ecad3957d043b63d9648809d4088db4c
Diego Atayra <diego@cycnova.com.ar>
2015-11-26 16:42:31 -0300

    filtrado de telegramas
    * filtro el telegrama si no coinciden la cantidad de votos válidos, o la cantidad de votos totales o la cantidad de votos positivos
    * para las pruebas solo lo limito a los primeros 500 telegramas
    * definí el toString del telegrama

--- elecciones.py
+++ elecciones.py
@@ -1,38 +1,34 @@
 class Telegrama:
     def __init__(self, line:str):
         parts = line.split(",")
-        self.Provincia = parts[0]
+        self.Provincia = int(parts[0])
         self.Departamento = parts[1]
         self.Circuito= parts[2]
-        self.Mesa= parts[3]
+        self.Mesa= int(parts[3])
         self.Telegrama= parts[4]
         self.Electores= parts[5]
-        self.TotalVotantes= parts[6]
-        self.VotosRecurridos= parts[7]
-        self.VotosImpugnados= parts[8]
-        self.VotosEnBlanco= parts[9]
-        self.VotosPositivos= parts[10]
-        self.VotosValidos= parts[11]
-        self.VotosCambiemos= parts[12]
-        self.VotosFPV= parts[13]
-
+        self.TotalVotantes= int(parts[6])
+        self.VotosRecurridos= int(parts[7])
+        self.VotosImpugnados= int(parts[8])
+        self.VotosEnBlanco= int(parts[9])
+        self.VotosPositivos= int(parts[10])
+        self.VotosValidos= int(parts[11])
+        self.VotosCambiemos= int(parts[12])
+        self.VotosFPV= int(parts[13])
+        
+    def __str__(self):
+        return "Provincia:{0},Departamento:{1},Circuito:{2},Mesa:{3},Electores:{4},TotalVotantes:{5},VotosRecurridos:{6},VotosImpugnados:{7},VotosEnBlanco:{8},VotosPositivos:{9},VotosValidos:{10},VotosCambiemos:{11},VotosFPV:{12}".format(self.Provincia, self. Departamento, self. Circuito, self. Mesa, self. Electores, self. TotalVotantes, self.VotosRecurridos, self.VotosImpugnados, self.VotosEnBlanco, self.VotosPositivos, self.VotosValidos, self.VotosCambiemos, self.VotosFPV)
+        
 def main():
     v = cargar_datos()
+    inconsistentes = filter(telegramaConsistente, v)
 
-'''
-previo a mostrar los telegramas filtraría por los que son inconsistentes.
-aplicar uno o varios filter a la colección previo definir qué es inconsistentes
-sugerencia:
-    VotosPositivos = VotosCambiemos + VotosFPV
-    VotosValidos = VotosPositivos + VotosEnBlanco
-    TotalVotantes = VotosRecurridos + VotosImpugnados + VotosEnBlanco + VotosEnPositivo
-    
-    inconsistentes = filter(inconsistencias, v)
-''' 
+    for telegrama in inconsistentes:
+        print(telegrama)
+    print("fin")
 
-    for telegrama in v:
-        print(telegrama.TotalVotantes)
-
+def telegramaConsistente(t):
+    return not((t.VotosFPV + t.VotosCambiemos == t.VotosPositivos) and (t.VotosEnBlanco + t.VotosPositivos == t.VotosValidos) and (t.TotalVotantes == t.VotosValidos + t.VotosImpugnados + t.VotosRecurridos))
 
 def cargar_datos():
     file = open("EscrutinioProvisorio.csv")
@@ -40,9 +36,12 @@ def cargar_datos():
     for line in file:
         if not line.startswith("Provincia") and not line.startswith(","):
             v.append(Telegrama(line))
+            
+        # limita la cantidad de telegramas
+        if len(v) == 500:
+            break
     file.close()
     return v
-
 
 if __name__ == '__main__':
     main()
